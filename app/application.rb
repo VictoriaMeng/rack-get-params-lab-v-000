2b8d8bf6c32009cc1f770483f249287699259fd1
Victoria Meng <nights1ststar@protonmail.com>
2018-05-17 02:15:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    req = Rack::Response.new(env)
+    req = Rack::Request.new(env)
 
     if req.path.match(/cart/)
       if @@cart.empty?
