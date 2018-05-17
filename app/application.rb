b81c802fbd22fb5c43c7bee1504c925d977180f0
Victoria Meng <nights1ststar@protonmail.com>
2018-05-17 02:17:13 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -17,19 +17,13 @@ class Application
       end
     elsif req.path.match(/add/)
       search_term = req.params["item"]
-      resp.write "#{handle_search(search_term)}"
+      if @@items.include?(search_term)
+      end
     end
 
 
     resp.finish
   end
 
-  def handle_search(search_term)
-    if @@items.include?(search_term)
-      return "#{search_term} is one of our items"
-    else
-      return "Couldn't find #{search_term}"
-    end
-  end
 
 end
