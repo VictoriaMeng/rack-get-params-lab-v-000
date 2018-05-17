7b0560659244ea2e243df397729b5062a194a189
Victoria Meng <nights1ststar@protonmail.com>
2018-05-17 02:09:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,9 @@ class Application
     if @@cart.empty?
       resp.write "Your cart is empty"
     else
+      @@cart.each do |item|
+        resp.write "#{item}\n"
+      end
     end
 
     resp.finish
